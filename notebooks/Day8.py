e81ff049af9a0b9a65d2e48627e55f6ca0597f1a
Parth Singh <parthsingh503@gmail.com>
2026-06-22 19:38:13 +0530

    Integrated tavily

---
+++ notebooks/Day8.py
@@ -0,0 +1,28 @@
+#Tavily Search AI Agent
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain.tools import tool
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.agents import create_agent
+from langchain_tavily import TavilySearch
+
+
+search = TavilySearch()
+model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+# print(search.invoke({"query": "Yesterday was FIFA enoguration, did nora fatehi also participated in the starting ceremony?"}))
+
+agent = create_agent(
+    model=model,
+    tools=[search],
+    system_prompt="You are a agent and can search for any question on google"
+)
+
+question = "What is happnening in the war iran and america latest news"
+
+response = agent.invoke({"messages": [{
+    "role": "user",
+    "content": question
+}]})
+
+print(response["messages"][-1].content[0]["text"])
