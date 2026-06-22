dd0d5d52c91907adfa3e00a492d57f6fd42211ce
Parth Singh <parthsingh503@gmail.com>
2026-06-22 20:19:23 +0530

    chatbot with search

---
+++ apps/2_google_search_agent.py
@@ -0,0 +1,76 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import streamlit as st
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.agents import create_agent
+from langchain_tavily import TavilySearch
+
+# LLM + Tool
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash"
+)
+
+search = TavilySearch()
+
+agent = create_agent(
+    model=model,
+    tools=[search],
+    system_prompt="You are a helpful AI assistant with web search capabilities."
+)
+
+st.set_page_config(
+    page_title="AskBuddy Search Agent",
+    page_icon="🤖"
+)
+
+st.title("🤖 AskBuddy Search Agent")
+st.markdown("AI chatbot with Gemini + Tavily Search")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat history
+for msg in st.session_state.messages:
+    with st.chat_message(msg["role"]):
+        st.markdown(msg["content"])
+
+query = st.chat_input("Ask Anything...")
+
+if query:
+
+    # Show user message
+    st.session_state.messages.append(
+        {
+            "role": "user",
+            "content": query
+        }
+    )
+
+    with st.chat_message("user"):
+        st.markdown(query)
+
+    try:
+        with st.chat_message("assistant"):
+            with st.spinner("Thinking..."):
+
+                response = agent.invoke(
+                    {
+                        "messages": st.session_state.messages
+                    }
+                )
+
+                ai_response = response["messages"][-1].content[0]["text"]
+
+                st.markdown(ai_response)
+
+        st.session_state.messages.append(
+            {
+                "role": "assistant",
+                "content": ai_response
+            }
+        )
+
+    except Exception as e:
+        st.error(f"Error: {str(e)}")
