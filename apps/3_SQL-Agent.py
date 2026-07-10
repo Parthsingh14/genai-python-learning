3bc027194c2d297f902970d4289625ef74fec492
Parth Singh <parthsingh503@gmail.com>
2026-07-10 15:57:24 +0530

    Conected with Postgres

--- apps/3_SQL-Agent.py
+++ apps/3_SQL-Agent.py
@@ -5,3 +5,5 @@ from langchain_community.utilities import SQLDatabase
 from langchain_community.agent_toolkits import SQLDatabaseToolkit
 from langchain_groq import ChatGroq
 
+db = SQLDatabase.from_uri("postgresql+psycopg2://postgres:postgres123@localhost:5432/my_tasks")
+print("DB created Successfully")
