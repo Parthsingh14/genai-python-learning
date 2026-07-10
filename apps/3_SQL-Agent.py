0290ad22bb4d551cfc77931db1a391fb04533587
Parth Singh <parthsingh503@gmail.com>
2026-07-10 16:55:10 +0530

    Initialized the tools and system prompt

--- apps/3_SQL-Agent.py
+++ apps/3_SQL-Agent.py
@@ -4,6 +4,38 @@ load_dotenv()
 from langchain_community.utilities import SQLDatabase
 from langchain_community.agent_toolkits import SQLDatabaseToolkit
 from langchain_groq import ChatGroq
+from langgraph.checkpoint.memory import InMemorySaver
 
 db = SQLDatabase.from_uri("postgresql+psycopg2://postgres:postgres123@localhost:5432/my_tasks")
-print("DB created Successfully")
+db.run("""
+CREATE TABLE IF NOT EXISTS tasks (
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT,
+    status TEXT CHECK (status IN ('pending', 'in_progress', 'completed')) DEFAULT 'pending',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+""")
+
+model = ChatGroq(model="qwen/qwen3-32b")
+
+toolkit = SQLDatabaseToolkit(db=db,llm=model)
+
+tools = toolkit.get_tools()
+
+memory = InMemorySaver()
+system_prompt = """
+    You are a task management assistant that interacts with a SQL database containing a 'tasks' table.
+    TASK RULES:
+    1. Limit SELECT queries  to 10 results max with ORDER BY created_at DESC
+    2. After CREATE/UPDATE/DELETE, confirm with SELECT query
+    3. If the user requests a lists of tasks, present the output in a structured table format to ensure a clean and organized display in the browser.
+
+    CRUD OPERATIONS:
+        CREATE: INSERT INTO task(title, description, status)
+        READ: SELECT * FROM task WHERE ... LIMIT 10
+        UPDATE: UPDATE tasks SET status=? WHERE id=? OR title=?
+        DELETE: DELETE FROM tasks WHERE id=? OR title=?
+    
+    Table schema: id,title,description, status(pending,in_progress,completed), created_at.
+"""
