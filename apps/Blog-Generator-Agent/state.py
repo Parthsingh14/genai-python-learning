43bf0565196c197b238fec53bda50a903d75881a
Parth Singh <parthsingh503@gmail.com>
2026-08-24 16:25:50 +0530

    Created the folder for the Multi agent blog generater and defined one node.

---
+++ apps/Blog-Generator-Agent/state.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+
+class BlogState(BaseModel):
+    ### User input
+    topic: str = ""
+    audience: str = ""
+
+    ### Researcher Output
+    research: str = ""
+    research_feedback: str = ""
+
+    ### Writer Output
+    draft: str = ""
+    draft_feedback: str = ""
+
+    ### Editor Output
+    final_draft: str = ""
+
+    ### Metadata
+    review_count: int = 0
+
